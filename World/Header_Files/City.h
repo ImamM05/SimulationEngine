0cb54226a0cdebfb32424d6bdb48505cc75cbb8b
ImamM05 <imammishkat@gmail.com>
2026-01-13 18:30:10 -0500

    defining base City struct

--- World/Header_Files/City.h
+++ World/Header_Files/City.h
@@ -1,12 +1,27 @@
 #ifndef CITY_H
 #define CITY_H
-#include <vector>
+#include <map>
 #include <string>
+#include <algorithm>
+
+// Base struct of City 
+// Subsequent City classes will inherit from this struct
 
 namespace World {
+    typedef std::map<std::string, int> FreqMap;
     struct City {
-        void visitLandMark();
-        std::vector<std::string> cityInventory;
+        FreqMap* cityInventory;
+        inline City(const FreqMap& inv) : cityInventory(new FreqMap(inv)) {};
+        inline ~City() {
+            delete cityInventory;
+        }
+        inline void invRemoveItem(std::string& item) {
+            
+        }
+        inline void invAddItem(std::string& item) {
+            (*cityInventory)[item]++;
+        }
+        virtual void visitLandMark() = 0;
     };
 }
 
